fee7a7ffc68dc7ab92a626520b9c8eb781fed629
aBraM <user@DESKTOP-N024CUM.localdomain>
2023-02-09 23:56:16 +0200

    improve setup_env.sh

--- git_utils/setup_env.sh
+++ git_utils/setup_env.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env sh
 
-NAME=$(git rev-parse --abbrev-ref HEAD);
-echo $NAME
+PROJECT_DIR=$(dirname "$( cd -- "$(dirname "$0")" >/dev/null 2>&1 || exit ; pwd -P )")
+HOOKS_DIR="$PROJECT_DIR"/.git/hooks
+echo "$PROJECT_DIR"
+
+for file in "$(dirname "$0")"/hooks/*
+do
+  echo "cp $file $HOOKS_DIR"
+  cp "$file" "$HOOKS_DIR"
+  echo "chmod +x $HOOKS_DIR/$(basename "$file")"
+  chmod +x "$HOOKS_DIR/$(basename "$file")"
+done
