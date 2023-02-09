5e8ebb5e2294d1c9a68a566173e915d683c8182f
aBraM <user@DESKTOP-N024CUM.localdomain>
2023-02-09 23:06:26 +0200

    init commit

---
+++ git_utils/prepare_commit_msg.py
@@ -0,0 +1,59 @@
+import os
+import sys
+import toml
+from pathlib import Path
+
+SCRIPT_PATH = Path(__file__).parent
+CONFIG_PATH = SCRIPT_PATH / "git_utils.toml"
+STDIN_FILENO = 1
+
+
+def unwrap(statement: bool, error_msg: str):
+    if not statement:
+        panic(error_msg)
+
+
+def panic(error_msg: str):
+    print(error_msg, file=sys.stderr)
+    exit(1)
+
+
+def read_config():
+    with open(CONFIG_PATH) as config_file_obj:
+        return dict((k.replace('-', '_'), v) for k, v in toml.load(config_file_obj).items())
+
+
+def force_present(msg: str):
+    unwrap(msg.split()[0][-2:] != "ed", f"Use present tense in commits! ({msg.split()[0]} is past tense)")
+
+
+def directory_prefix(msg: str):
+    pass
+
+
+def get_status():
+    pass
+
+
+ACTIONS = {"directory_prefix": directory_prefix,
+           "force_present": force_present}
+
+
+def main():
+    msg_path, cmdline = sys.argv[1:]
+    with open(msg_path) as msg_file_obj:
+        msg = msg_file_obj.read()
+    cmdline = cmdline.split()[1:]
+    sys.stdin.close()
+    sys.stdin = os.fdopen(STDIN_FILENO)
+
+    config = read_config()
+
+    if "-m" in cmdline:
+        for action in ACTIONS:
+            if config[action]:
+                ACTIONS[action](msg)
+
+
+if __name__ == '__main__':
+    main()
---
+++ git_utils/setup_env.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+
+NAME=$(git rev-parse --abbrev-ref HEAD);
+echo $NAME
