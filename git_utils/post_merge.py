87318112310637a7fb29db342f1ebba458e60df4
aBraM <user@DESKTOP-N024CUM.localdomain>
2023-02-10 02:22:16 +0200

    # This is a combination of 2 commits. # This is the 1st commit message:
    add config structure
    
    # This is the commit message #2:
    
    add str

--- git_utils/post_merge.py
+++ git_utils/post_merge.py
@@ -11,7 +11,7 @@ STDIN_FILENO = 1
 
 def read_config():
     with open(common.CONFIG_PATH) as config_file_obj:
-        return dict((k.replace('-', '_'), v) for k, v in toml.load(config_file_obj).items())["prepare_commit_msg"]
+        return dict((k.replace('-', '_'), v) for k, v in toml.load(config_file_obj).items())
 
 
 ACTIONS = {"force_present": actions.force_present,
@@ -21,23 +21,12 @@ ACTIONS = {"force_present": actions.force_present,
 
 
 def main():
-    msg_path, cmdline = sys.argv[1:]
-    with open(msg_path) as msg_file_obj:
-        msg = msg_file_obj.read()
+    is_squash, cmdline = sys.argv[1:]
     cmdline = cmdline.split()[1:]
     sys.stdin.close()
     sys.stdin = os.fdopen(STDIN_FILENO)
 
     config = read_config()
-    print(config)
-
-    if "-m" in cmdline:
-        with open(msg_path, 'r+') as msg_file_obj:
-            commit = Commit(msg, config)
-            for action in ACTIONS:
-                if action in config.keys():
-                    commit = ACTIONS[action](commit)
-            common.set_msg(commit.msg, msg_file_obj)
 
 
 if __name__ == '__main__':
